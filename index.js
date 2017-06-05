5b4997c2df5f1899ff68fe21cf37a33aa7f53572
Matthew Reynolds <matthew_reynolds@tsco.org>
2017-06-05 16:27:22 -0400

    Added route for api

---
+++ index.js
@@ -0,0 +1,20 @@
+
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.get('/api', function (req, res) {
+  res.send('{"api":"apidata"}');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
